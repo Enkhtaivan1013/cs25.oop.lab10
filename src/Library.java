0347209ed3118e3e7f3ad8966443913e8f49faca
by-bilgn <by.bilguun22@gmail.com>
2025-04-29 11:01:03 +0800

    Update files with new changes

--- src/Library.java
+++ src/Library.java
@@ -1,45 +1,43 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 class Book {
-	private String title;
-	private int year;
+    private String title;
+    private int year;
 
-	public Book(String title, int year) {
-		this.title = title;
-		this.year = year;
-	}
+    public Book(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
 
-	public String getTitle() {
-		return title;
-	}
-
-	public int getYear() {
-		return year;
-	}
+    public String getTitle() { return title; }
+    public int getYear() { return year; }
 }
 
 public class Library {
-	private List<Book> books = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
 
-	public void addBook(String title, int year) {
-		books.add(new Book(title, year));
-	}
+    public void addBook(String title, int year) {
+        books.add(new Book(title, year));
+    }
 
-	class BookShelf implements Iterable<Book> {
-		private int minYear;
+    class BookShelf implements Iterable<Book> {
+        private int minYear;
 
-		public BookShelf(int minYear) {
-			this.minYear = minYear;
-		}
+        public BookShelf(int minYear) {
+            this.minYear = minYear;
+        }
 
-		@Override
-		public Iterator<Book> iterator() {
-			// Хэрэгжүүл: minYear-с хойшхи номнуудыг буцаа
-			return null; // Оруул
-		}
-	}
+        @Override
+        public Iterator<Book> iterator() {
+            return books.stream()
+                .filter(book -> book.getYear() >= minYear)
+                .iterator();
+        }
+    }
 
-	public BookShelf getRecentBooks(int minYear) {
-		return new BookShelf(minYear);
-	}
+    public BookShelf getRecentBooks(int minYear) {
+        return new BookShelf(minYear);
+    }
 }
